f616540bea657ace968ca2ab89a0c0e6769a2f7d
Faycal <rwigemafayk@gmail.com>
2023-11-16 15:32:19 -0800

    trying to hit API with fetch()

--- client/src/components/RelatedProducts.jsx
+++ client/src/components/RelatedProducts.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 
-const RelatedProducts = () => (
-    <div>
+// require('dotenv').config();
 
-    </div>
-);
+const RelatedProducts = () => {
+  const options = { Authorization: process.env.PORT, URL: process.env.URL };
+  const fetch = () => {
+    axios.get(options.URL, options)
+      .then((data) => {
+        console.log(data);
+        console.log(process.env.PORT);
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+        <div>
+          {console.log(process.env.PORT)}
+            Testing - Related products: {fetch()};
+        </div>
+  );
+};
 
 export default RelatedProducts;
