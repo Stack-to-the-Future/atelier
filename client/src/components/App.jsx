87c0c049fa9eb0943ceb28cec79192c6bd9383c0
greg <greg.jenkins1034@gmail.com>
2023-11-16 17:55:53 -0600

    Commiting first draft, Overview component + styles.css

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Overview from './Overview.jsx';
 import RelatedProducts from './RelatedProducts.jsx';
 import QandA from './QandA.jsx';
+// import Styles from '../dist/styles.css';
 
 const App = () => (<div>
   Hello Everyone!
+  <Overview />
   <RelatedProducts />
   <QandA />
 </div>
---
+++ client/src/components/Overview.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Overview = () => (
+  <div>
+    <div><h1>LOGO</h1><h1>Search Bar...</h1></div>
+    <div>
+    <h2>Here some ANNOUNCMENTS --  SALE BIG DISCOUNT -- New Product Highlight</h2>
+    </div>
+  </div>
+);
+
+export default Overview;
