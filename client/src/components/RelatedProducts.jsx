649f323b4c28e3bc45d03febc6cf8326b881ff9e
Faycal <rwigemafayk@gmail.com>
2023-11-16 19:01:47 -0800

    createroot rendering and getRequest

--- client/src/components/RelatedProducts.jsx
+++ client/src/components/RelatedProducts.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import axios from 'axios';
 
-// require('dotenv').config();
-
 const RelatedProducts = () => {
-  const options = { Authorization: process.env.PORT, URL: process.env.URL };
+  // const headers = { headers: { Authorization: process.env.TOKEN } };
+  const options = { headers: { Authorization: process.env.TOKEN } };
+  const url = process.env.URL;
   const fetch = () => {
-    axios.get(options.URL, options)
+    axios.get(url, options)
       .then((data) => {
-        console.log(data);
-        console.log(process.env.PORT);
+        console.log(data.data);
       })
       .catch((err) => console.log(err));
   };
   return (
         <div>
-          {console.log(process.env.PORT)}
-            Testing - Related products: {fetch()};
+            -Testing - Related products: {fetch()};
         </div>
   );
 };
