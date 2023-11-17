b82ff84793b3784f40feff181ad4894c8a72da1e
greg <greg.jenkins1034@gmail.com>
2023-11-16 18:17:48 -0600

    Fixed merge conflict with RatingsAndReviews import statement.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Overview from './Overview.jsx';
 import RelatedProducts from './RelatedProducts.jsx';
 import QandA from './QandA.jsx';
+import RatingsAndReviews from './RatingsAndReviews.jsx';
 // import Styles from '../dist/styles.css';
 
 const App = () => (<div>
