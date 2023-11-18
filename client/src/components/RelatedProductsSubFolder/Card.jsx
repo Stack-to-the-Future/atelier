0d59b048b94ad916a31677b39670261cda5ae876
Faycal <rwigemafayk@gmail.com>
2023-11-17 22:52:30 -0800

    product details

--- client/src/components/RelatedProductsSubFolder/Card.jsx
+++ client/src/components/RelatedProductsSubFolder/Card.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, {} from 'react';
 
-const Card = () => (
-  <div>
-
-  </div>
+const Card = ({ product }) => (
+    <>
+      <img
+         src=""
+         width="100"
+         height="170"
+      />
+      <h3>CATEGORY</h3>
+      <h4>{product.category}</h4>
+      <h4>{product.name}</h4>
+      <h5>{product.description}</h5>
+      <h4>{product.default_price}</h4>
+    </>
 );
 
 export default Card;
--- client/src/components/RelatedProductsSubFolder/RelatedProductsList.jsx
+++ client/src/components/RelatedProductsSubFolder/RelatedProductsList.jsx
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Card from './Card.jsx';
 
 const RelatedProductsList = () => {
   const [products, setProducts] = useState([]);
+  // const [product, setProduct] = useState({});
   const [relatedProductsId, setRelatedProductsId] = useState([]);
+  // const [category, setCategory] = useState('');
+  // const [name, setName] = useState('');
+  // const [price, setPrice] = useState('');
+  // const [ID, setID] = useState(0);
   // const headers = { headers: { Authorization: process.env.TOKEN } };
   const options = { headers: { Authorization: process.env.TOKEN } };
   // useEffect for when component mounts
@@ -16,23 +22,30 @@ const RelatedProductsList = () => {
       .catch((err) => console.log(err));
     // get related items
     axios.get(`${process.env.URL}/products/40346/related`, options)
-      .then((related) => {
-        setRelatedProductsId(related.data);
-        console.log('related Items::::', relatedProductsId);
-      });
+      .then((data) => {
+        setRelatedProductsId(data.data);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
   return (
     <div>
       <h5>RELATED PRODUCTS</h5>
+      <button><h4>◀︎</h4></button>
       <ul>
-        {
-          products.filter((p) => relatedProductsId.includes(p.id))
-            .map((p, index) => <li key={index}> {p.name} </li>)
-        }
-      </ul>
+          {
+            products.filter((p) => relatedProductsId.includes(p.id))
+              .map((product, index) => (
+                <li key={index}>
+                  <Card product={product} relatedProductsId={relatedProductsId} />
+                  </li>
+              ))
+          }
+        </ul>
+        <button><h4>◀︎</h4></button>
+
     </div>
   );
 };
-
+//                  // category={category} name={name} price={price} ID={ID} />
 export default RelatedProductsList;
