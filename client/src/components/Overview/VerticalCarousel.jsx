cb7b6f2ebf0455eefcc14799e59ecd96c2149d9c
Ming Lee Ng <mingleeng@gmail.com>
2023-11-21 16:58:21 -0700

    add: implementation of vertical carousel

---
+++ client/src/components/Overview/VerticalCarousel.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Overview.css';
+
+const VerticalCarousel = ({ photos, photoIdx, setPhotoIdx }) => (
+  <div id="overview-gallery-styles">
+    {photos.map((photo, idx) => (
+      <img
+        src={photo.url}
+        key={idx}
+        onClick={() => setPhotoIdx(idx)}
+        className={`overview-gallery-style ${idx === photoIdx ? 'overview-gallery-selected' : ''
+          }`}
+      />
+    ))}
+  </div>
+);
+
+export default VerticalCarousel;
