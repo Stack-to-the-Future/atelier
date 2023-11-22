eb3547cb0d088c3427387f0dc3d0954e5435b3f9
Faycal <rwigemafayk@gmail.com>
2023-11-21 16:00:48 -0800

    photos and relProds  matched

--- client/src/components/RelatedProductsSubFolder/Card.jsx
+++ client/src/components/RelatedProductsSubFolder/Card.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, {} from 'react';
 import './RelPro.css';
 
-const Card = ({ product, photo }) => (
+const Card = ({ product, productKey }) => {
+  console.log('ZZZZZZZ', productKey);
+
+  return (
     <button className='card-button' style={ {
       marginLeft: 15, marginBottom: 15, backgroundColor: 'lightbrown', padding: 25,
     } }>⭐️
       <br/>
       <img
-         src={`${photo}`}
+         src={product[productKey].photo}
          width="100"
          height="150"
       />
       <br/>
-      <h4>{product.category}</h4>
+      <h4>{product[productKey].category}</h4>
       <br/>
-      <h4 className='prod-name'>{product.name}</h4>
-      <h4>${product.default_price}</h4>
+      <h4 className='prod-name'>{product[productKey].name}</h4>
+      <h4>${product[productKey].price}</h4>
     </button>
 
-);
+  );
+};
 
 export default Card;
--- client/src/components/RelatedProductsSubFolder/Carousel.jsx
+++ client/src/components/RelatedProductsSubFolder/Carousel.jsx
@@ -1,12 +1,61 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import Card from './Card.jsx';
 
-const Carousel = ({ relatedProducts, photo }) => {
+const Carousel = ({ relatedProducts }) => {
   const length = 3;
   const [index, setIndex] = useState(0);
-  const [displayed, setDisplayed] = useState([...relatedProducts.slice(0, length)]);
+  const [displayedProducts, setDisplayedProducts] = useState([]);
+  // const [currentProducts, setCurrentProducts] = useState([relatedProducts]);
+  const options = { headers: { Authorization: process.env.TOKEN } };
 
   const scroll = (direction) => {
+    const newIndex = index + direction;
+    setIndex(newIndex);
+    // console.log('.......', temp);
+
+    for (let i = 0; i < relatedProducts.length; i += 1) {
+      axios.get(`${process.env.URL}/products/${relatedProducts[i].id}/styles`, options)
+        .then((data) => {
+          const prod = relatedProducts[i].id;
+          // console.log(prod);
+          const temp = {};
+          temp[prod] = {
+            name: relatedProducts[i].name,
+            category: relatedProducts[i].category,
+            price: relatedProducts[i].default_price,
+            photo: data.data.results[0].photos[0].thumbnail_url,
+          };
+          console.log('.......', temp);
+          setDisplayedProducts([...displayedProducts, temp]);
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+  console.log('.......', displayedProducts);
+
+  return (
+    <div id='rel-prod-carousel'>
+     {(index >= 0) ? <button onClick={() => scroll(-1)}><h4>◀︎ BACK</h4></button> : <div></div> }
+          {
+            displayedProducts.slice(index, index + length).map((product, idx) => (
+              <Card key={idx} product={ product } productKey={Object.keys(product)[0]} />
+            ))
+          }
+     {index < (relatedProducts.length - length)
+       ? <button onClick={() => scroll(+1)}><h4>NEXT ▶︎</h4></button>
+       : <div></div> }
+    </div>
+  );
+};
+
+export default Carousel;
+
+/*
+
+<Card key={idx} product={{ ...product, photo: photos[product.id] }} />
+
+const scroll = (direction) => {
     const newIndex = index + direction;
     if (newIndex < 0) {
       setIndex(relatedProducts.length - 1);
@@ -23,17 +72,34 @@ const Carousel = ({ relatedProducts, photo }) => {
     setDisplayed([...copy]);
   };
 
-  return (
-    <div id='rel-prod-carousel'>
-      <button onClick={() => scroll(-1)}><h4>◀︎ BACK</h4></button>
-          {
-            displayed.map((product, idx) => (
-                <Card key={idx} product={product} photo={photo}/>
-            ))
-          }
-        <button onClick={() => scroll(+1)}><h4>NEXT ▶︎</h4></button>
-    </div>
-  );
-};
+*/
 
-export default Carousel;
+/*
+
+    for (let i = 0; i < relatedProducts.length; i += 1) {
+      axios.get(`${process.env.URL}/products/${relatedProducts[i].id}/styles`, options)
+        .then((data) => {
+          // relatedProducts[i].photo = data.data.results[0].photos;
+          // setCurrentData([...relatedProducts.map
+          ((product) => ({ ...product, photo: data.data.results[0].photos }))]);
+          setCurrentData
+          ([...currentData, { ...relatedProducts[i], photo: data.data.results[0].photos }]);
+        })
+        .catch((err) => console.log(err));
+    }
+
+*/
+
+/*
+
+              // setCurrentData([...relatedProducts.map
+              ((product) => ({ ...product, photo: data.data.results[0].photos }))]);
+
+          // setCurrentData([...relatedProducts.
+          map((product) => ({ ...product, photo: data.data.results[i].photos[0] }))]);
+
+          // setCurrentData
+          ([...currentData, { ...relatedProducts[i], photo: data.data.results[0].photos[0] }]);
+          // console.log('.......', data.data.results[0].photos[0].thumbnail_url)
+
+*/
