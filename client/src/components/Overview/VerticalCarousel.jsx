72a5153a27e6453fe85a40a756b1cce89dc2c79e
Ming Lee Ng <mingleeng@gmail.com>
2023-11-22 18:34:58 -0700

    fix: vertical carousel key issues
    Fixed vertical carousel key issues by ensuring that all keys are unique without utilizing a mapped idx

--- client/src/components/Overview/VerticalCarousel.jsx
+++ client/src/components/Overview/VerticalCarousel.jsx
@@ -1,25 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Overview.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
-const VerticalCarousel = ({ photos, photoIdx, setPhotoIdx }) => (
-  <div id="overview-gallery-styles">
-    {photos.map((photo, idx) => (
-      <button
-        type="button"
-        key={`photo:${photo.url.slice(0, 3)}`}
-        onClick={() => setPhotoIdx(idx)}
-      >
-        <img
-          src={photo.url}
-          className={`overview-gallery-style ${idx === photoIdx ? 'overview-gallery-selected' : ''
+const VerticalCarousel = ({ photos, photoIdx, setPhotoIdx }) => {
+  const [index, setIndex] = useState(0);
+  const size = 3;
+
+  const handleClick = (direction) => setIndex(index + direction);
+
+  return (
+    <div id="overview-gallery-styles">
+      {index > 0 ? (
+        <button
+          type="button"
+          onClick={() => handleClick(-1)}
+          className="overview-gallery-style vert-btn"
+        >
+          <FontAwesomeIcon icon={faChevronUp} className="icon gallery-icon" />
+        </button>
+      ) : (
+        <div className="overview-gallery-style vert-btn gallery-invisible" />
+      )}
+      {photos.slice(index, index + size).map((photo, idx) => (
+        <button
+          className={`overview-gallery-style ${idx + index === photoIdx ? 'overview-gallery-selected' : ''
           }`}
-          alt={`style:${idx}`}
-        />
-
-      </button>
-
-    ))}
-  </div>
-);
-
+          type="button"
+          key={`${photo.url}`}
+          onClick={() => setPhotoIdx(idx + index)}
+        >
+          <img
+            className="overview-gallery-image"
+            src={photo.url}
+            alt={`style:${idx}`}
+          />
+        </button>
+      ))}
+      {index < photos.length - size ? (
+        <button
+          type="button"
+          onClick={() => handleClick(1)}
+          className="overview-gallery-style vert-btn"
+        >
+          <FontAwesomeIcon icon={faChevronDown} className="icon gallery-icon" />
+        </button>
+      ) : (
+        <div className="overview-gallery-style vert-btn gallery-invisible" />
+      )}
+    </div>
+  );
+};
 export default VerticalCarousel;
