b24d913f24d0305ef80b1f53fb59e236816e7aa9
Thomas Harbert <tgharbert3@gmail.com>
2023-11-28 12:44:20 -0600

    cleaned up App component

--- tests/QandA/answer.test.js
+++ tests/QandA/answer.test.js
@@ -5,18 +5,18 @@ import Answer from '../../client/src/components/QandASubFolder/QuestionsList.jsx
 
 const answer = {
   answer_id: 5993312,
-  body: "Unfortunately with sweatshops...",
-  date: "2023-11-23T00:00:00.000Z",
-  answerer_name: "thombombadil420",
+  body: 'Unfortunately with sweatshops...',
+  date: '2023-11-23T00:00:00.000Z',
+  answerer_name: 'thombombadil420',
   helpfulness: 6,
-  photos: []
-}
+  photos: [],
+};
 
 describe('it should render Answer properly', () => {
   beforeEach(() => render(<Answer answer={answer} />));
 
   it('should render an Answer', async () => {
-    const modal = await screen.findByTestId("answer");
+    const modal = await screen.findByTestId('answer');
     expect(modal).toBeTruthy();
-  })
-})
+  });
+});
