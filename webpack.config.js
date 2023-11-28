d2ba8a1105ee7923610018102ea874653dba490d
Ming Lee Ng <mingleeng@gmail.com>
2023-11-28 01:02:46 -0700

    removed unnecessary comments

--- webpack.config.js
+++ webpack.config.js
@@ -2,82 +2,34 @@ const path = require('path');
 const webpack = require('webpack');
 require('dotenv').config({ path: './.env' });
 
-/* This tells webpack to take index.js from entry. Then check for all file extensions in resolve.
-After that apply all the rules in module.rules and produce the output and place it in main.js */
-
 module.exports = {
-  /** "mode"
-   * the environment - development, production, none. tells webpack
-   * to use its built-in optimizations accordingly. default is production
-   */
   mode: 'development',
-  /** "entry"
-   * the entry point
-   */
   entry: './client/src/index.jsx',
   output: {
-    /** "path"
-     * the folder path of the output file
-     */
     path: path.resolve(__dirname, './client/dist'),
-    /** "filename"
-     * the name of the output file
-     */
     filename: 'main.js',
   },
-  /** "target"
-   * setting "node" as target app (server side), and setting it as "web" is
-   * for browser (client side). Default is "web"
-   */
   target: 'web',
   devServer: {
-    /** "port"
-     * port of dev server
-     */
     port: process.env.PORT,
-    /** "static"
-     * This property tells Webpack what static file it should serve
-     */
     static: ['./client/dist'],
-    /** "open"
-     * opens the browser after server is successfully started
-     */
     open: true,
-    /** "hot"
-     * enabling and disabling HMR. takes "true", "false" and "only".
-     * "only" is used if enable Hot Module Replacement without page
-     * refresh as a fallback in case of build failures
-     */
     hot: true,
-    /** "liveReload"
-     * disable live reload on the browser. "hot" must be set to false for this to work
-     */
     liveReload: true,
   },
   resolve: {
-    /** "extensions"
-     * If multiple files share the same name but have different extensions, webpack will
-     * resolve the one with the extension listed first in the array and skip the rest.
-     * This is what enables users to leave off the extension when importing
-     */
     extensions: ['.js', '.jsx', '.json'],
   },
   module: {
-    /** "rules"
-     * This says-"Hey webpack compiler, when you come across a path that resolves to a '.js or .jsx'
-     * file inside of a require()/import statement, use the babel-loader to transform it before you
-     * add it to the bundle. In this process, kindly make sure to exclude node_modules folder from
-     * being searched"
-     */
     rules: [
       {
-        test: /\.(js|jsx)$/, // kind of file extension this rule should look for and apply in test
-        exclude: /node_modules/, // folder to be excluded
-        use: 'babel-loader', // loader which we are going to use
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
       },
       {
-        test: /\.css$/i, // kind of file extension this rule looks for
-        use: ['style-loader', 'css-loader'], // loader being used (its for react component css)
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
