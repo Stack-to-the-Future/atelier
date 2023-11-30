0dd7124ec9fe8afd47706295e492cacd04321cd1
Faycal <rwigemafayk@gmail.com>
2023-11-29 19:51:00 -0800

    save and pull

---
+++ tests/RelatedProducts/relatedProducts.test.js
@@ -0,0 +1,4 @@
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import '@testing-library/jest-dom';
+// import RelatedProducts from '../../client/src/components/RelatedProducts.jsx';
