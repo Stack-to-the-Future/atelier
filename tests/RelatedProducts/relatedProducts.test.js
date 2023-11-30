2de8eae44cea32d072ff67835060ec2a31c4d468
Faycal <rwigemafayk@gmail.com>
2023-11-29 20:27:21 -0800

    testing tests

--- tests/RelatedProducts/relatedProducts.test.js
+++ tests/RelatedProducts/relatedProducts.test.js
@@ -1,4 +1,29 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import '@testing-library/jest-dom';
-// import RelatedProducts from '../../client/src/components/RelatedProducts.jsx';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import RelatedProducts from '../../client/src/components/RelatedProducts.jsx';
+
+describe('RelatedProducts Component', () => {
+  it('should render the RelatedProducts component', async () => {
+    render(
+      <RelatedProducts
+        products={[
+          {
+            id: 40344,
+            campus: 'hr-rfp',
+            name: 'Camo Onesie',
+            slogan: 'Blend in to your crowd',
+            description:
+              `The So Fatigues will wake you up and fit you in.
+    This high energy camo will have you blending in to even the wildest surroundings.`,
+            category: 'Jackets',
+            default_price: '140.00',
+            created_at: '2021-08-13T14:38:44.509Z',
+            updated_at: '2021-08-13T14:38:44.509Z',
+          },
+        ]}
+      />,
+    );
+    await expect(screen.queryByText('RELATED PRODUCTS')).toBeTruthy();
+  });
+});
