d140fba71467394a4d19f72f2877a6c9df35e7a8
Thomas Harbert <tgharbert3@gmail.com>
2023-11-30 22:31:03 -0600

    cleaned up modal design

--- client/src/components/QandASubFolder/AnswerForm.jsx
+++ client/src/components/QandASubFolder/AnswerForm.jsx
@@ -22,28 +22,34 @@ const AnswerForm = ({
           </h4>
           <form onSubmit={submitAnswer}>
             {/* BODY */}
-            <label htmlFor="answer-body" className="modal-label">
-              Your Answer (mandatory)*
-              <div>
-                <textarea type="text" className="modal-answer" maxLength="1000" value={body} onChange={modalFunctions.bodyChange} />
-              </div>
-            </label>
+            <div className="field-container">
+              <label htmlFor="answer-body" className="modal-label">
+                Your Answer (mandatory)*
+                <div>
+                  <textarea type="text" className="modal-input" maxLength="1000" value={body} onChange={modalFunctions.bodyChange} />
+                </div>
+              </label>
+            </div>
             {/* USERNAME */}
-            <label htmlFor="answer-username" className="modal-label">
-              What is your nickname? (mandatory)*
-              <div>
-                <input type="text" className="modal-input" placeholder="Example: jack543!" maxLength="60" value={username} onChange={modalFunctions.usernameChange} />
-                <p>For privacy reasons, do not use your full name or email address</p>
-              </div>
-            </label>
+            <div className="field-container">
+              <label htmlFor="answer-username" className="modal-label">
+                What is your nickname? (mandatory)*
+                <div>
+                  <input type="text" className="modal-input" placeholder="Example: jack543!" maxLength="60" value={username} onChange={modalFunctions.usernameChange} />
+                  <p className="sub-text">For privacy reasons, do not use your full name or email address</p>
+                </div>
+              </label>
+            </div>
             {/* EMAIL */}
-            <label htmlFor="answer-email" className="modal-label">
-              Your email (mandatory)*
-              <div>
-                <input type="text" className="modal-input" placeholder="Example: jack@email.com" maxLength="60" value={email} onChange={modalFunctions.emailChange} />
-                <p>For authentication reasons, you will not be emailed</p>
-              </div>
-            </label>
+            <div className="field-container">
+              <label htmlFor="answer-email" className="modal-label">
+                Your email (mandatory)*
+                <div>
+                  <input type="text" className="modal-input" placeholder="Example: jack@email.com" maxLength="60" value={email} onChange={modalFunctions.emailChange} />
+                  <p className="sub-text">For authentication reasons, you will not be emailed</p>
+                </div>
+              </label>
+            </div>
             {/* INVALID FIELD INFO */}
             <button className="submission" type="button" onClick={() => showSetPhotoForm(true)}>Add Photos</button>
             <button className="submission" type="button" onClick={submitAnswer}>Add Answer</button>
